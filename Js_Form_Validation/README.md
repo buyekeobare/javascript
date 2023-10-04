1e3f2d7744106f2866feb78081a6233bdd6ea181
buyekeobare <buyekeobare@gmail.com>
2023-10-04 15:53:51 +0300

    Updated files

---
+++ Js_Form_Validation/README.md
@@ -0,0 +1,16 @@
+## Create the project structure
+
+## Build HTML Form
+
+### Select the form fields and add the submit event listener
+
+### Develop utility functions
+
+### Develop functions that show the error / success
+
+### Develop input field validating functions
+
+1. Validate the username field
+2. Validate the email field
+3. Validate the password field
+4. Validate the confirm password field
---
+++ Js_Form_Validation/js/app.js
@@ -0,0 +1,13 @@
+const usernameE1 = document.querySelector('#name');
+const useremailE1 = document.querySelector('#email');
+const passwordE1 = document.querySelector('#password');
+const confirmPasswordE1 = document.querySelector('#confirm-password');
+
+const form = document.querySelector('#signup');
+
+form.addEventListener('submit', function (e) {
+  // prevent the form from submitting
+  e.preventDefault();
+  
+});
+
