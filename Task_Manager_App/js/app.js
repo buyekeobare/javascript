87df24ea5fdf36bf2b3d95f9cc3105cbcf6d4074
buyekeobare <buyekeobare@gmail.com>
2023-11-19 11:03:40 +0300

    Updated file

---
+++ Task_Manager_App/js/app.js
@@ -0,0 +1,21 @@
+import { validateForm, getActivityFields, reset } from './utility.js'
+
+const taskBtn = document.querySelector("#task-form-btn");
+const tasks = [];
+
+taskBtn.addEventListener("click", function() {
+  const isValidated = validateForm();
+
+  if(!isValidated)
+    return;
+
+  const fieldValues = getActivityFields();
+  reset();
+
+  tasks.push(fieldValues);
+  console.log(fieldValues)
+})
+
+const createTaskItems = () => {
+  
+}
